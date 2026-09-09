e40c9d69174b45a065586afeb19302a4a0e1a72b
Santiagosaltoss <saltosantiago26@outlook.com>
2026-09-08 23:08:46 -0300

    Tabla  Ventas creada

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float, Boolean, Date, Time, ForeignKey
 from database import Base 
+from sqlalchemy.sql import func
 
 class Producto(Base):
     __tablename__ = 'productos'
@@ -7,4 +8,14 @@ class Producto(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     nombre = Column(String(50), nullable=False) 
     precio = Column(Float, nullable=False)
+
+class Ventas(Base):
+    __tablename__ = 'ventas'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    fecha = Column(Date, default=func.current_date())
+    hora = Column(Time, nullable=False)
+    id_producto = Column(Integer, ForeignKey('productos.id'), nullable=False)
+    cantidad = Column(Integer, nullable=False)
+    precio_total = Column(Float, nullable=False)
 
